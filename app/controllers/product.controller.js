6c92f60044ba9ffa3a067f9458f5526101412e43
Melina PM <meli.pm@gmail.com>
2022-11-09 09:29:37 -0300

    create a product

--- app/controllers/product.controller.js
+++ app/controllers/product.controller.js
@@ -3,8 +3,31 @@ const Product = db.products;
 
 // Create and Save a new Product
 exports.create = (req, res) => {
-  
+   // Validate request
+   if (!req.body.title) {
+    res.status(400).send({ message: "Content can not be empty!" });
+    return;
 };
+
+// Create a Tutorial
+const tutorial = new Tutorial({
+title: req.body.title,
+description: req.body.description,
+published: req.body.published ? req.body.published : false
+});
+
+// Save Tutorial in the database
+tutorial
+.save(tutorial)
+.then(data => {
+    res.send(data);
+})
+.catch(err => {
+    res.status(500).send({
+    message:
+        err.message || "Some error occurred while creating the Tutorial."
+    });
+});
 
 // Retrieve all Products from the database.
 exports.findAll = (req, res) => {
