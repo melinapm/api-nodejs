9c75675775ca83d9961868b6a7d94f479d188ea9
Melina PM <meli.pm@gmail.com>
2022-11-09 13:02:48 -0300

    Update db.config.js

--- app/config/db.config.js
+++ app/config/db.config.js
@@ -1,5 +1,5 @@
 module.exports = {
-    url: "mongodb+srv://seminarioNoSql:v5dp7LJm9aJ0kYba@clustermongodb.u961qxu.mongodb.net/?retryWrites=true&w=majority"
+    url: "mongodb+srv://<user>:<pass>@clustermongodb.u961qxu.mongodb.net/?retryWrites=true&w=majority"
   };
 
 
